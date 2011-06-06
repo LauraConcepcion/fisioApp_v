ddffda4abcf2bafefefffd57553eb8157450e0c0
Usabi <usabi@MacBook-Pro-de-Laura-Concepcion.local>
2011-06-06 17:48:23 +0100

    creamos el proyecto con la base de datos de postgres y el gemfile actualizado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FisioApp::Application.config.secret_token = 'fffb832528d23e1ecba4af641e55d3b48d08d43ee7ea970992b7f02db51b50673c75a082caaa4cffb3e0df4b8ae332416d939d59d8138ba5e25dab282fb66876'
