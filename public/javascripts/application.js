732fdd832eb1e4e015688f261e881ced9fd4c66b
Usabi <usabi@MacBook-Pro-de-Laura-Concepcion.local>
2011-06-22 20:47:46 +0100

    modificamos los estilos de los formularios

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function (){  
-    $('#paciente_birthdate').datepicker({ changeYear: true ,yearRange: '1950:2010' });
+    $('#paciente_birthdate').datepicker({ changeYear: true ,yearRange: '1950:2010'});
     $('#clinicalhistory_assessmentdate').datepicker();
     $('#clinicalhistory_startdatetto').datepicker({ altFormat: 'yy-mm-dd' });
     $('#clinicalhistory_enddatetto').datepicker({ altFormat: 'yy-mm-dd' });
