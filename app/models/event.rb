9acd073205a273cb4d00ffaa49cb9eda8dc94046
Usabi <usabi@MacBook-Pro-de-Laura-Concepcion.local>
2011-06-22 20:47:23 +0100

    modificamos el modelo de events para incluir las relaciones con centro y paciente

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible  :starttime, :endtime, :specialist, :center, :patient
+  attr_accessible  :starts_at, :ends_at, :all_day, :description, :center_id, :specialist_id
  
   belongs_to  :center
   belongs_to  :specialist
