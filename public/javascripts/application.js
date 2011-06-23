6aba5786cfe5143d30f1e77cc0b88985336fcf20
Usabi <usabi@MacBook-Pro-de-Laura-Concepcion.local>
2011-06-23 14:13:23 +0100

    modificamos el formato del calendario

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function (){  
-    $('#paciente_birthdate').datepicker({ changeYear: true ,yearRange: '1950:2010'});
-    $('#clinicalhistory_assessmentdate').datepicker();
-    $('#clinicalhistory_startdatetto').datepicker({ altFormat: 'yy-mm-dd' });
-    $('#clinicalhistory_enddatetto').datepicker({ altFormat: 'yy-mm-dd' });
+    $('#paciente_birthdate').datepicker({ dateFormat: 'dd-mm-yy' ,yearRange: '1950:2010'});
+    $('#clinicalhistory_assessmentdate').datepicker({ dateFormat: 'dd-mm-yy' });
+    $('#clinicalhistory_startdatetto').datepicker({ dateFormat: 'dd-mm-yy' });
+    $('#clinicalhistory_enddatetto').datepicker({ dateFormat: 'dd-mm-yy' });
 });
