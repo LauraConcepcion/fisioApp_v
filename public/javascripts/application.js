806df33964e5a82ecfb4bea488f4ac2d4b9931c8
Usabi <usabi@MacBook-Pro-de-Laura-Concepcion.local>
2011-06-23 14:15:56 +0100

    añadimos el link desde el menu a las citas

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function (){  
-    $('#paciente_birthdate').datepicker({ dateFormat: 'dd-mm-yy' ,yearRange: '1950:2010'});
+    $('#paciente_birthdate').datepicker({ dateFormat: 'dd-mm-yy' , changeYear: true,yearRange: '1950:2010'});
     $('#clinicalhistory_assessmentdate').datepicker({ dateFormat: 'dd-mm-yy' });
     $('#clinicalhistory_startdatetto').datepicker({ dateFormat: 'dd-mm-yy' });
     $('#clinicalhistory_enddatetto').datepicker({ dateFormat: 'dd-mm-yy' });
